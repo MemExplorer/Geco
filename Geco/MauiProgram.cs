d928a27d27ca70f84f92e1ff590c37f53eceee1c
MemExplorer <132466484+MemExplorer@users.noreply.github.com>
2024-10-19 22:27:28 +0800

    Implemented basic flyout navigation
    - Added font 'Poppins-Regular'
    - Modified current navigation to flyout navigation

--- Geco/MauiProgram.cs
+++ Geco/MauiProgram.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Handlers;
 
 namespace Geco
 {
@@ -15,13 +16,33 @@ namespace Geco
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Poppins-Regular.ttf", "Poppins");
                 });
 
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
+            // platform specific modifications
+            ApplyAndroidModifications();
 
             return builder.Build();
+        }
+
+        static void ApplyAndroidModifications()
+        {
+            ToolbarHandler.Mapper.AppendToMapping("CustomNavigationView", static (handler, view) =>
+            {
+                if (DeviceInfo.Platform == DevicePlatform.Android)
+                {
+                    // Original Code: handler.PlatformView.ContentInsetStartWithNavigation = 0;
+                    // Compiler complains about not resolving ContentInsetStartWithNavigation in .NET 9 Preview.
+                    // Check in december again if it's still an issue when .NET 9 release is out
+                    // For now, we cope with this hack solution
+                    var platform = handler.PlatformView;
+                    var propertyItem = platform.GetType().GetProperty("ContentInsetStartWithNavigation");
+                    propertyItem?.SetValue(platform, 0);
+                }
+            });
         }
     }
 }
