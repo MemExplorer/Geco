6dcc38c38058a5aa7a9181303c96957217370b41
MemExplorer <132466484+MemExplorer@users.noreply.github.com>
2024-12-19 17:23:14 +0800

    Fix Battery Observer Trigger Logic

--- Geco/Platforms/Android/ActionObservers/BatteryStateObserver.cs
+++ Geco/Platforms/Android/ActionObservers/BatteryStateObserver.cs
@@ -15,15 +15,12 @@ internal class BatteryStateObserver : IDeviceStateObserver
 		double batteryInfo = Battery.Default.ChargeLevel;
 		double chargeLevel = batteryInfo * 100;
 		bool isCharging = Battery.Default.State == BatteryState.Charging;
-		DeviceInteractionTrigger triggerType;
 
 		// Check if the battery percentage when charging is outside the range of 20-80%
-		if (isCharging && Battery.Default.PowerSource != BatteryPowerSource.Battery &&
-		    (chargeLevel < 20 || chargeLevel > 80))
-			triggerType = DeviceInteractionTrigger.ChargingUnsustainable;
-		else
-			triggerType = DeviceInteractionTrigger.ChargingSustainable;
-
-		OnStateChanged?.Invoke(sender, new TriggerEventArgs(triggerType, e));
+		if (isCharging && Battery.Default.PowerSource != BatteryPowerSource.Battery)
+		{
+			var triggerType = chargeLevel is < 20 or > 80 ? DeviceInteractionTrigger.ChargingUnsustainable : DeviceInteractionTrigger.ChargingSustainable;
+			OnStateChanged?.Invoke(sender, new TriggerEventArgs(triggerType, e));
+		}
 	}
 }
