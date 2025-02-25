ade31e628919b0294925e3929b6a43719cdeb32b
MemExplorer <132466484+MemExplorer@users.noreply.github.com>
2025-02-25 16:20:45 +0800

    Add unit test for prompt builder

--- Geco.Core.Test/DatabaseTest.cs
+++ Geco.Core.Test/DatabaseTest.cs
@@ -47,9 +47,46 @@ public class DatabaseTest
 		string dbPath = Environment.CurrentDirectory;
 		var promptRepo = new PromptRepository(dbPath);
 
-		//await promptRepo.PurgeTable();
-
 		string triggerPrompt = await promptRepo.GetPrompt(DeviceInteractionTrigger.BrowserUsageUnsustainable);
 		_output.WriteLine($"Trigger Prompt: {triggerPrompt}");
+	}
+
+	[Fact]
+	async Task PromptBuilderTest()
+	{
+		string dbPath = Environment.CurrentDirectory;
+		var promptRepo = new PromptRepository(dbPath);
+
+		var weeklyReportNoPrevPrompt = await promptRepo.GetLikelihoodPrompt(30, "", "");
+		var weeklyReportWithPrevPrompt = await promptRepo.GetLikelihoodWithHistoryPrompt(30, "", 30, "", "");
+		var triggerNotifPrompt = await promptRepo.GetPrompt(DeviceInteractionTrigger.ChargingUnsustainable);
+
+		Debug.Assert(!new string[] {
+			"CurrentSustainabilityLikelihood",
+			"CurrentLikelihoodComputation",
+			"SustainabilityLevel"
+		}
+		.All(x => weeklyReportWithPrevPrompt.Contains($"{{{x}}}")));
+
+		Debug.Assert(!new string[] {
+			"CurrentSustainabilityLikelihood",
+			"CurrentLikelihoodComputation",
+			"PreviousSustainabilityLikelihood",
+			"PreviousLikelihoodComputation",
+			"SustainabilityLevel"
+		}
+		.All(x => weeklyReportNoPrevPrompt.Contains($"{{{x}}}")));
+
+		_output.WriteLine($"Weekly Report: Pass");
+
+		Debug.Assert(!new string[] {
+			"ActionTrigger",
+			"SustainableBaselineData",
+			"StoredPromptRefinement"
+		}
+		.All(x => triggerNotifPrompt.Contains($"{{{x}}}")));
+
+		_output.WriteLine($"Notification: Pass");
+
 	}
 }
